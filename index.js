a31467329ff64fc2583c549d50346dfcdbd858ec
saraiesco <saraiescobar@google.com>
2024-01-24 11:26:42 -0600

    complete modules

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ app.use(express.urlencoded({extended:true}));
   // define the config object
   // attach Auth0 OIDC auth router
   // create a GET / route handler that sends back Logged in or Logged out
+  const { auth } = require('express-openid-connect');
+
+
+  const config = {
+    authRequired: true,
+    auth0Logout: true,
+    secret: process.env.AUTH0_SECRET,
+    baseURL: process.env.AUTH0_AUDIENCE,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH0_BASE_URL,
+  };
+  
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  app.use(auth(config));
+  
+  // req.isAuthenticated is provided from the auth router
+  app.get('/', (req, res) => {
+    console.log(req.oidc.user)
+    const user = req.oidc.user;
+    res.send(req.oidc.isAuthenticated() ? `Welcome, ${user.name} Username: ${user.email}` : 'Logged out');
+  });
+
 
 app.get('/cupcakes', async (req, res, next) => {
   try {
